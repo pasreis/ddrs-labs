44bb04b5878e412fde502ff1947dc1736dd86277
ist197969 <enrygiorio97@gmail.com>
2020-12-01 17:43:56 +0000

    finished ex2

--- lab4/ex2.R
+++ lab4/ex2.R
@@ -1,57 +1,189 @@
 # CONSTANTS
-ARRIVAL1 = 1
-ARRIVAL2 = 2
-TRANSMISSION = 3
 
 # PARAMETERS
-arrivalRate = c(4,9)
+arrivalRate = c(4,5)
 avgPacketSize = c(500, 170)
-quantum = c(1500,900)
-n = 1000
+quantum = c(1000,2000)
+n = 100
 capacity=4000
 
 # VARIABLES
 EventList <- rep(Inf, 3)
 packets = 0
 activeQueue = 1
-first = c()
-second = c()
-transChannel = c()
-queue = list(first,second,transChannel)
+queue = list(c(),c())
+queueCount = rep(0,2)
+transmissionOngoing = FALSE
 time = 0
 
 # STATISTICAL COUNTERS
-deficit1 = 0
-deficit2 = 0
+deficit = c(quantum[1],0)
+totTransmitted = rep(0,2)
 
 EventList[1] = rexp(1,arrivalRate[1])
 EventList[2] = rexp(1,arrivalRate[2])
 packets = packets + 2
 
-while(packets < n){
-    nextEvent = which.min(EventList)
-
-    if(nextEvent == ARRIVAL1){
-        list[ARRIVAL1] = c(list[ARRIVAL1],min(EventList))
-        EventList[1] = rexp(1,arrivalRate[1])
-
-    } else if (nextEvent == ARRIVAL2){
-        list[ARRIVAL2] = c(list[ARRIVAL2],min(EventList))
-        EventList[2] = rexp(1,arrivalRate[2])
-        
-
-    } else { # TRANSMISSION FROM LINK
-        
-        if(activeQueue == 1){
-            deficit1 = deficit1 + quantum1
-            nextPacket = runif(1, avgSize1*2)
-            while(<=deficit1){
-                deficit1 = deficit1 - nextPacket
-                print(paste("1 - ", nextPacket))
-            }
-        }
-
+switchQueue = function() {
+    activeQueue <<- ifelse(activeQueue == 1, 2, 1)
+    if(length(queue[[activeQueue]]) > 0 ) {
+        deficit[activeQueue] <<- deficit[activeQueue] + quantum[activeQueue]
+        print(paste("switch to queue ", activeQueue, " new deficit: ",deficit[activeQueue], 
+    " first packet size: ", queue[[activeQueue]][1]))
+    } else {
+        deficit[activeQueue] <<- 0
+        print(paste("switch to queue ", activeQueue, " new deficit: 0, no packets in the queue"))
     }
-    
 
 }
+
+transmitPacket = function() {
+    transmissionOngoing <<- TRUE;
+    print(paste("Transmission from queue ", activeQueue, 
+    " Deficit= ", deficit[activeQueue]," first packet= ",queue[[activeQueue]][1]))
+
+    deficit[activeQueue] <<- deficit[activeQueue] - queue[[activeQueue]][1]
+    print(paste("New Deficit = ",deficit[activeQueue]))
+
+    EventList[3] <<- time + queue[[activeQueue]][1] / capacity 
+    queue[[activeQueue]] <<- queue[[activeQueue]][-1]
+    queueCount[activeQueue] <<- queueCount[activeQueue] -1
+    totTransmitted[activeQueue] <<- totTransmitted[activeQueue] +1
+    
+}
+
+
+while(packets < n){
+    nextEvent = which.min(EventList)
+    print(EventList)
+    print(paste("Next event = ", nextEvent))
+    time = time + min(EventList)
+
+    if(nextEvent != 3){
+        
+        size = runif(1,min = 0, max = 2*avgPacketSize[nextEvent]) #calculate random size
+        EventList[nextEvent] = rexp(1,arrivalRate[nextEvent]) +time
+        packets = packets+1
+
+        if(queueCount[nextEvent] == 0 && activeQueue == nextEvent 
+            && transmissionOngoing == FALSE && deficit[nextEvent] >= size){
+            # here I don't touch any queue
+            EventList[3] = size / capacity +time
+            transmissionOngoing = TRUE
+            totTransmitted[activeQueue] = totTransmitted[activeQueue] +1
+            deficit[activeQueue] = deficit[activeQueue] - size
+            print(paste(size, " arrives in queue ", activeQueue, ", empty link and enough deficit: GO"))
+            #print(paste("It will take ",EventList[3])) #-time
+
+        } else if(queueCount[nextEvent] == 0 && activeQueue == nextEvent 
+                    && transmissionOngoing == FALSE && deficit[nextEvent]<size){
+            # if the reason of the client going in the queue is JUST that the channel's deficit is not enough
+            # then I switch queue until I don't have enough transmission deficit
+            queue[[nextEvent]] = c(queue[[nextEvent]],size)
+            queueCount[nextEvent] = queueCount[nextEvent] + 1
+
+            # the && in the while condition server a purpose:
+            # suppose a queue has not enough deficit and the second queue is empty
+            # the switch would set to zero the second queue's deficit (bacause it's empty)
+            # and the condition could make the loop terminate 
+            # (extracting the first element of an empty vector is dubious)
+            # by adding this condition we're covered from any shenanigan
+            # as the loop always terminates on the non empty queue when it has enough deficit
+            print(paste(size," arrives in queue ", activeQueue, ", empty link but not enough deficit:"))
+
+           # I keep switching and adding until I have enough deficit in one of the queues
+           # as long as at least one queue has some packets waiting
+            nextSize = ifelse(queueCount[activeQueue]>0, queue[[activeQueue]][1], Inf)
+
+            while(deficit[activeQueue] < nextSize && (queueCount[1]>0 || queueCount[2]>0)){
+                switchQueue()
+                # putting queue[[activeQueue]][1] in the condition makes it explode if the queue is empty
+                # so I put Inf meaning that the control will switch again due to true condition
+                # until the original queue has enough deficit
+
+                nextSize = ifelse(queueCount[activeQueue]>0, queue[[activeQueue]][1], Inf)
+            }
+
+            # maybe now they're both 0 so I have to check
+            if(queueCount[activeQueue]!=0)
+                transmitPacket()
+            else{
+                print("No more packets waiting, idle channel")
+                transmissionOngoing=FALSE
+            }
+            
+        }
+        else { # any other case put in the queue
+            # other cases include: 
+            # arrival on non active queue, transmission already ongoing, other packets already in queue
+            print(paste(size, " arrives in queue ", nextEvent, 
+            " QUEUED because activeQueue=",activeQueue, " , transmission: ",transmissionOngoing,
+            " remaining deficit: ",deficit[nextEvent]))
+            queue[[nextEvent]] = c(queue[[nextEvent]],size)
+            queueCount[nextEvent] = queueCount[nextEvent] + 1
+            print(paste("There are ",queueCount[nextEvent], " clients in queue", nextEvent))
+        }
+    } else { # TRANSMISSION FROM LINK
+
+        if (queueCount[activeQueue]>0 && deficit[activeQueue]>=queue[[activeQueue]][1]){
+            print(paste("Other packet on queue ",activeQueue, " ready"))
+            # other packets ready to be transmitted from the same queue
+            transmitPacket()
+
+        } else if (queueCount[activeQueue]>0 && deficit[activeQueue] < queue[[activeQueue]][1]){
+            print(paste("other packets ready but deficit is inferior to the first packet size (",
+            deficit[activeQueue],"<",queue[[activeQueue]][1],")"))
+            
+            #print(paste("Queue", activeQueue, ": ",queueCount[1]))
+            #print(queue[[activeQueue]])
+            nextSize = ifelse(queueCount[activeQueue]>0, queue[[activeQueue]][1], Inf)
+
+            while(deficit[activeQueue] < nextSize && (queueCount[1]>0 || queueCount[2]>0)){
+                switchQueue()
+                # putting queue[[activeQueue]][1] in the condition makes it explode if the queue is empty
+                # so I put Inf meaning that the control will switch again due to true condition
+                # until the original queue has enough deficit
+
+                nextSize = ifelse(queueCount[activeQueue]>0, queue[[activeQueue]][1], Inf)
+            }
+
+            if(queueCount[activeQueue]!=0)
+                transmitPacket()
+            else
+            {
+                transmissionOngoing=FALSE
+                print("Not transmitting, no clients in queue")
+            }
+                
+
+        } else { #if(queueCount[activeQueue] == 0) {
+            print(paste("No more packets in queue ", activeQueue, 
+            ": set deficit=0 and try the other one"))
+            deficit[activeQueue] = 0
+            switchQueue()
+
+            if(queueCount[activeQueue] == 0){ # no more packets in the other queue as well
+                print("Not transmitting, no clients in any queue")
+                deficit[activeQueue] = 0
+                transmissionOngoing = FALSE
+                EventList[3] = Inf
+
+            } else { # in case the other queue has some clients
+                print(paste("There are ",queueCount[activeQueue], " packets waiting here"))
+                if(deficit[activeQueue] >= queue[[activeQueue]][1]) {
+                    transmitPacket()
+                } else {
+                    print("But not enough deficit")
+                    # no reason to switch every time because the other queue is empty in this case
+                    while(deficit[activeQueue] <= queue[[activeQueue]][1]){
+                        deficit[activeQueue] = deficit[activeQueue] + quantum[activeQueue]
+                    }
+                    print(paste("Increased the deficit of ",activeQueue, " to ",deficit[activeQueue],
+                    " to transmit the first packet because the other queue is empty"))
+                    transmitPacket()
+                }
+            }
+        }
+    }
+}
+
